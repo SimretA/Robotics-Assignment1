05264831667c49f0e606a9333516894f9505d452
Zekaryas1 <zekaryasdinku@gmail.com>
2021-06-16 15:12:03 -0700

    fixed script bug and we need to source setup.bash

--- src/arm2_gazebo/scripts/script.bash
+++ src/arm2_gazebo/scripts/script.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 sleep 4
-rosrun arm2_gazebo 0 -2.5 0.1 G
-rosrun arm2_gazebo 0 -2.5 0.1 C
-rosrun arm2_gazebo 0 -2.5 0.1 R
-rosrun arm2_gazebo 0 -2.5 0.1 G 
+source ./devel/setup.bash
+rosrun arm2_gazebo ik.py 0 -2.5 0.1 G
+rosrun arm2_gazebo ik.py 0 -2.5 0.1 C
+rosrun arm2_gazebo ik.py 0 -2.5 0.1 R
+rosrun arm2_gazebo ik.py 0 -2.5 0.1 G 
